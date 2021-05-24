1ed4ef9bc3b1d36bb5dab4f2770577575a729a6a
Krzysztof Krawczyk <44506319+kkrawczykpl@users.noreply.github.com>
2021-05-24 19:57:14 +0200

    Shape interface, tests refactor

--- geometry/geometry_test.go
+++ geometry/geometry_test.go
@@ -2,22 +2,25 @@ package main
 
 import "testing"
 
-func TestPerimeter(t *testing.T) {
-	rectangle := Rectangle{10.0, 15.0}
-	got := Perimeter(rectangle)
-	want := 50.0
-
-	if got != want {
-		t.Errorf("Got: %.2f, want: %.2f", got, want)
-	}
-}
-
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{10.0, 15.0}
-	got := Area(rectangle)
-	want := 150.0
 
-	if got != want {
-		t.Errorf("Got: %.2f, want: %.2f", got, want)
+	areaTests := []struct {
+		shape Shape
+		want float64
+	} {
+		{Rectangle{10.0, 15.0}, 150.0},
+		{Circle{10}, 314.1592653589793},
+	}
+
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	}
+
+	for _, testCase := range areaTests {
+		checkArea(t, testCase.shape, testCase.want)
 	}
 }
